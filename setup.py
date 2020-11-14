6929c08cf51030837c51b3f343d003d1c1e068ae
andreasxp <andres.zhukov@gmail.com>
2020-11-14 20:54:20 +0300

    Remove useless stuff from setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-install_requires = []
-extras_require = {}
-
 setup(
     name="overload",
     version="0.1",
@@ -10,8 +7,5 @@ setup(
     author="Andrey Zhukov",
     author_email="andres.zhukov@gmail.com",
     license="MIT",
-    install_requires=install_requires,
-    package_data={},
-    extras_require=extras_require,
     packages=["overload"],
 )
