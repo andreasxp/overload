037b487b8dc7315a92ab960213c03211ab7d092a
andreasxp <andres.zhukov@gmail.com>
2021-10-23 13:04:18 +0300

    Remove useless __init__ file

--- src/__init__.hpp
+++
@@ -1,8 +0,0 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
-#include <iostream>
-#include <vector>
-#include "exceptions.hpp"
-#include "hello.hpp"
-#include "util/ref.hpp"
-#include "util/macros.hpp"
